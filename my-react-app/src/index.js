1c2c1716590e683f5565720c8fdc01388a0c3237
divyagehlot31 <divyagehlot3112@gmail.com>
2025-06-10 18:48:35 +0530

    Creating a React app with Webpack

---
+++ my-react-app/src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom/client';
+import React from 'react';
+
+const App = () => <h1>Hello from React!</h1>;
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
